0e3c1190a4adeea0338b04590646889715d398bf
agzuniverse <aswinganesh666@gmail.com>
2020-08-13 17:13:02 +0530

    Add ticker endpoint

---
+++ pkg/handlers/ticker.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Excel-MEC/excelplay-backend-dalalbull/pkg/database"
+	"github.com/Excel-MEC/excelplay-backend-dalalbull/pkg/env"
+	"github.com/Excel-MEC/excelplay-backend-dalalbull/pkg/httperrors"
+)
+
+// Ticker returns details of all stocks to display in a ticker
+func Ticker(db *database.DB, env *env.Config) httperrors.Handler {
+	return func(w http.ResponseWriter, r *http.Request) *httperrors.HTTPError {
+		var tickerData []database.TickerData
+		err := db.GetTickerData(&tickerData)
+		if err != nil {
+			return &httperrors.HTTPError{r, err, "Could not get ticker data", http.StatusInternalServerError}
+		}
+		jsonRes, err := json.Marshal(tickerData)
+		if err != nil {
+			return &httperrors.HTTPError{r, err, "Could not serialize json", http.StatusInternalServerError}
+		}
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonRes)
+		return nil
+	}
+}
