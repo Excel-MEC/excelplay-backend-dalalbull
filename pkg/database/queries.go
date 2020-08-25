7e23a84786b0b0b26c0be6cdc5bef94ec1f97135
agzuniverse <aswinganesh666@gmail.com>
2020-08-25 18:44:41 +0530

    Remove queries that were not used anymore

--- pkg/database/queries.go
+++ pkg/database/queries.go
@@ -46,26 +46,6 @@ func (db *DB) GetTopLosers(topLosers *StockChange) error {
 	return db.Get(topLosers, "select symbol, change_per from stocks_data order by change_per limit 5")
 }
 
-// // GetHints gets the hints released for a question
-// func (db *DB) GetHints(currLev int, hints *[]string) error {
-// 	return db.Select(hints, "select content from hints where number = $1", currLev)
-// }
-
-// // LogAnswerAttempt logs every answer attempt
-// func (db *DB) LogAnswerAttempt(uuid string, currUser User, answer string) (sql.Result, error) {
-// 	return db.Exec("insert into answer_logs values($1, $2, $3, $4)", uuid, currUser.Name, answer, time.Now())
-// }
-
-// // GetCorrectAns gets the correct answer for a level from the DB to check if the user's attempt is correct
-// func (db *DB) GetCorrectAns(currUser User, correctAns *string) error {
-// 	return db.Get(correctAns, "select answer from levels where number = $1", currUser.CurrLevel)
-// }
-
-// // CorrectAnswerSubmitted increments the user level on submission of correct answer
-// func (db *DB) CorrectAnswerSubmitted(uuid string) (sql.Result, error) {
-// 	return db.Exec("update duser set curr_level = curr_level + 1 where id = $1", uuid)
-// }
-
 // GetLeaderboard gets the users list in the descending order of level,
 // and for users on the same level, in the ascending order of last submission time.
 func (db *DB) GetLeaderboard(users *[]User) error {
