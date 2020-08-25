00353f97d46ab3a89d4c16307619967f2d3de666
agzuniverse <aswinganesh666@gmail.com>
2020-08-25 18:20:53 +0530

    Add structs to hold stock data for various endpoints

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -64,6 +64,16 @@ func (router *Router) Routes(db *database.DB, config *env.Config) {
 			),
 		),
 	).Methods("GET")
+	router.Handle("/api/dashboard",
+		middlewares.ErrorsMiddleware(
+			httperrors.Handler(
+				middlewares.AuthMiddleware(
+					handlers.GetDashboard(db, config),
+					config,
+				),
+			),
+		),
+	).Methods("GET")
 
 	// LoggerMiddleware does not have to be selectively applied because it applies to all endpoints
 	router.Use(middlewares.LoggerMiddleware)
