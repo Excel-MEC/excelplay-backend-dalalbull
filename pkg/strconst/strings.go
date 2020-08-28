e9fc8c754af39a025110b171ebd3e1bac6cad297
agzuniverse <aswinganesh666@gmail.com>
2020-08-28 15:31:28 +0530

    Add string constant for invalid json error

--- pkg/strconst/strings.go
+++ pkg/strconst/strings.go
@@ -5,4 +5,5 @@ const (
 	MalformedToken = "Malformed Token"
 	Unauthorized   = "User is not authorized"
 	ClaimFail      = "Failed to validate claims"
+	InvalidJSON    = "Could not decode JSON data"
 )
